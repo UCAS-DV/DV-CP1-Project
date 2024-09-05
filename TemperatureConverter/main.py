a993fe7d2bc812c0f16cf2c93a5ed0c0dfe630d7
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-09-04 18:54:43 -0600

    Created "TemperatureConverter" to convert temperatures from Celsuis to fahrenheit.

---
+++ TemperatureConverter/main.py
@@ -0,0 +1,6 @@
+# Darius Vaiaoga, "ProficiencyTest: Celsius to Fahrenheit", Period 2
+temp_celsius = int(input("What is the temperature in Celsius: "))
+
+temp_fahrenheit = (temp_celsius * 9/5) + 32
+
+print(f"{temp_celsius} degrees celsius is also {temp_fahrenheit} degrees fahrenheit")
