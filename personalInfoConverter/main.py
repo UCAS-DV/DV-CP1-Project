aedfe156caf8b3b7b82e18f3756e92313afb9665
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-09-19 10:51:06 -0600

    Create Personal Info Gatherer

---
+++ personalInfoConverter/main.py
@@ -0,0 +1,8 @@
+# Darius Vaiaoga, "ProficiencyTest: Personal Information Converter", Period 2
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+height = float(input("How tall are you (in inches)? "))
+fav_number = int(input("And most importantly, what is your favorite integer? "))
+
+print(f"Alright, thanks for the info. You are {name}. You're {age} years old and {height} inches tall. Finally, your favorite number is {fav_number}")
