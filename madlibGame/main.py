bc0dde8adfec9e43c4e916950b6eeceb13c82338
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-09-19 10:35:27 -0600

    Finished the Madlib Game by removing two of the three paths and by adding the final sentences to the remaining path.

--- madlibGame/main.py
+++ madlibGame/main.py
@@ -5,32 +5,21 @@ intro = "Hello! Welcome to Madlibs! You will be given 5 sentences and you're goi
 
 print(intro)
 
-first_sentences = [
-    "Josh, James, and Jacob were hanging out, plotting to ____.",
-    'The presidential debate was tense, but despite his opponants attacks, the incumbant proudly declared "_____!"',
-    'Mark was finally going to ask Sarah, "_____ with me?"'
-]
+first_sentence = "Josh, James, and Jacob were hanging out, plotting to ____."
+first_word = input(first_sentence + " Fill in the blank: ")
 
-words = ['','','','']
+second_sentence = '"It was brilliant, and nothing could possibly go wrong!" They thought, until they realized that _____ would disapprove'
+second_word = input(second_sentence + " Fill in the blank: ")
 
-sentence_choice = 0
+third_sentence = f'"Are you boys planning to {first_word} again?" {second_word} asked but they already knew the answer. So they _____ to avoid punishment'
+third_word = input(third_sentence + " Fill in the blank: ")
 
-words[0] = input(first_sentences[sentence_choice] + " Fill in the blank: ").lower
+forth_sentence = f'Finally now they could put their plan into action at the _____.'
+forth_word = input(forth_sentence + " Fill in the blank: ")
 
-second_sentences = [
-    '"It was brilliant, and nothing could possibly go wrong!" They thoughts, until they realized that _____ would disaprove',
-    f'""{words[0]}" are you kidding me?" his rival responded. To further prove his point he _____ to the despair of the incumbant.',
-    '"Yes! I love doing that!" She proudly proclaimed but before they could start, _____ stopped them dead in their tracks.'
-]
+fifth_sentence = f'But before they {first_word} at the {forth_word}, {second_word} appeared and _____ them. The End!'
+fifth_word = input(fifth_sentence + " Fill in the blank: ")
 
-third_sentences = [
-    f'"Are you boys planning to {words[0]} again?" they asked but they already knew the answer. So they _____ to avoid punishment'
-]
+final_story = f"Josh, James, and Jacob were hanging out, plotting to {first_word}. 'It was brilliant, and nothing could possibly go wrong!' They thoughts, until they realized that {second_word} would disapprove. 'Are you boys planning to {first_word} again?' {second_word} asked but they already knew the answer. So they {third_word} to avoid punishment. Finally, now they could put their plan into action at the {forth_word}. But before they could {first_word} at the {forth_word}, {second_word} appeared and {fifth_word} them"
 
-final_stories = [
-    f'Josh, James, and Jacob were hanging out, plotting to {words[0]}. It was brilliant, and nothing could possibly go wrong!" They thoughts, until they realized that {words[1]} would disaprove',
-]
-
-words[1] = input(f"{second_sentences[sentence_choice]} Fill in the blank: ").lower
-
-print(f'Your final story is: "{final_stories[sentence_choice]}"')
+print(f'Your final story is: "{final_story}"')
