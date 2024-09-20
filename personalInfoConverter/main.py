b747fe96c6c99d3403c0b8b689c9dfafdfa1ed9c
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-09-20 10:11:58 -0600

    Completed the personalInfoConverter so that it prints both the converted and non converted values

--- personalInfoConverter/main.py
+++ personalInfoConverter/main.py
@@ -1,8 +1,13 @@
 # Darius Vaiaoga, "ProficiencyTest: Personal Information Converter", Period 2
 
 name = input("What is your name? ")
-age = int(input("How old are you? "))
-height = float(input("How tall are you (in inches)? "))
-fav_number = int(input("And most importantly, what is your favorite integer? "))
+age = input("How old are you? ")
+height = input("How tall are you (in meters)? ")
+fav_number = input("And most importantly, what is your favorite integer? ")
 
-print(f"Alright, thanks for the info. You are {name}. You're {age} years old and {height} inches tall. Finally, your favorite number is {fav_number}")
+casted_age = int(age)
+casted_height = float(height)
+casted_fav_number = int(fav_number)
+
+print(f"You told me, in strings that, you're {name}; you're '{age}' years old; you're '{height}' meters tall and you're favorite number is '{fav_number}'")
+print(f"After converting, I can now see that you're name is still a string which is, {name}. You're age in years is the integer, {casted_age}. You're height in meters is the floating point number, {casted_height}. And your favorite number is definitely an integer, which is {casted_fav_number}.")
