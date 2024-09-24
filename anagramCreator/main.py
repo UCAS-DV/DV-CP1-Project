2984f5d60dededb50737effe61b9fe7c87578210
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-09-24 10:49:54 -0600

    Started work on anagram creator

---
+++ anagramCreator/main.py
@@ -0,0 +1,14 @@
+import random
+
+def GenerateOrder(length):
+    iteration = 0
+    order = {-1}
+    random_num = random.randint(0, length-1)
+    while iteration <= length:
+        
+
+def GenerateAnagram(word):
+    for letter in word:
+        anagram = word[random.randint(0,word.len() - 1)]
+    return anagram
+
