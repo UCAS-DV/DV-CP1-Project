b0305bf029a0ab493af30f5c05918fecd5dad35d
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-09-25 10:50:14 -0600

    Started work on a CeaserCypher. Problems with letters overflowing

--- anagramCreator/main.py
+++ anagramCreator/main.py
@@ -2,13 +2,21 @@ import random
 
 def GenerateOrder(length):
     iteration = 0
-    order = {-1}
+    order = [-1]
     random_num = random.randint(0, length-1)
-    while iteration <= length:
-        
+    while iteration < length:
+        if iteration == 0:
+            order[0] = random_num
+        else:
+            order.append(random_num)
+        iteration += 1
+        print(order)
+            
 
 def GenerateAnagram(word):
     for letter in word:
         anagram = word[random.randint(0,word.len() - 1)]
     return anagram
+
+GenerateOrder(7)
 
---
+++ secretCypher/main.py
@@ -0,0 +1,58 @@
+letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    'Y',
+    'Z'
+]
+    
+def shiftLetter(character, shift):
+    if character != " ":
+        character_value = ord(character)
+        if (character_value + shift) > 90:
+            character = 65
+            shift = 90 - character_value
+            return(chr(character + shift))
+        return chr(character_value + shift)
+    else:
+        return character   
+
+def EncodeMessage(message="", inputed_shift=0):
+    encoded_message = ""
+    for char in message.upper():
+        encoded_message += shiftLetter(char, inputed_shift)
+    return encoded_message
+
+while True:
+    user_shift = -1
+    user_message = input("What message would you like to encode? ")
+    while user_shift < 0 or user_shift > 25:
+        user_shift = int(input("How far do you want to shift the message? (shift cannot be less than 0 or more than 25) "))
+
+    print(f"You chose to encode {user_message} with a shift of {user_shift}.")
+    print(f"You're encoded message is: {EncodeMessage(user_message, user_shift)}")
+
+    if input('Do you want to encode more messages? If so, type anything, if not, type "_exit" ') == "_exit":
+        exit()
+        
+
