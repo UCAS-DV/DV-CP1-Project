aafe2af2aa743fe270fbb8c19c91352dd496e084
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-10-02 10:05:13 -0600

    yippee!

---
+++ Sep30-Oct4/listOfClasses/main.py
@@ -0,0 +1,15 @@
+# Darius Vaiaoga, "SkillPractice: List of Classes", Period 2
+
+classes = [
+    "Geography", 
+    "Programming Basics", 
+    "Biology", 
+    "Student Council",
+    "Secondary Math 2",
+    "Directed Studies",
+    "English 9"]
+
+print("My classes are... ")
+
+for entry in classes:
+    print(entry)
