ee07b372edcac4739b4b34020ead60f19051e020
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-10-02 10:50:17 -0600

    Ra

--- Aug23-Sep27/pigLatinTranslator/main.py
+++ Aug23-Sep27/pigLatinTranslator/main.py
@@ -1,14 +1,24 @@
-def Translate(word):
+vowels = [
+    "a",
+    "e",
+    "i",
+    "o",
+    "u"
+]
+
+def Translate(word=""):
     translated_word = ""
-    translated_word = word[1:] 
-    print(translated_word)     
-    translated_word += word.lower()[0] + "ay"
+    vowel_location = 1
+    for letter in word:
+        if letter in vowels:
+            vowel_location = word[word.find(letter)]
+    print(vowel_location)
+    translated_word = word[vowel_location:] + word[:vowel_location] + "ay"
 
     return translated_word
 
 while True:
-
-    output = input("What word do you want to translate into Pig Latin? ")
-    if output == "_stop":
+    user_input = input("What word do you want to translate into Pig Latin? ")
+    if user_input == "_stop":
         exit()
-    print(f'"{output}" in Pig Latin is "{Translate(output)}"')
+    print(f'"{user_input}" in Pig Latin is "{Translate(user_input)}"')
