9bd55573faafa6d231962bba63e637e1870213e4
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-10-04 10:13:21 -0600

    It's finally working!

--- Sep30-Oct4/gradeCalculator/main.py
+++ Sep30-Oct4/gradeCalculator/main.py
@@ -3,19 +3,13 @@
 
 num_of_classes = int(input("How many classes do you have? "))
 
-grades = [-1,-1,-1,-1,-1,-1,-1,-1]
+grades = [0]
 
 grades_entered = False
 class_num = 1
 
 total_grade_value = 0
 grade_average = 0
-
-if num_of_classes < 1 or num_of_classes > 8:
-    while num_of_classes < 1 or num_of_classes > 8:
-        num_of_classes = int(input("Oh! Sorry! You have to enter at least one class and less than 9 classes. Please try again."))
-
-
 
 while not grades_entered:
     
@@ -33,16 +27,12 @@ while not grades_entered:
     if class_num % 10 >= 4:
         ordinal = f"{class_num}th"
     
-    grades[class_num - 1]+= int(input(f"What is your grade (in %) in your {ordinal} class? "))
+    grades.append(int(input(f"What is your grade (in %) in your {ordinal} class? ")))
     class_num += 1
 
 for grade in grades:
-    if grade == -1:
-        grade_average = total_grade_value / num_of_classes
-        break
-
-    print(grade)
-    total_grade_value += grades[grade]
+    total_grade_value += grade
+    grade_average = total_grade_value / num_of_classes
 
     
 
