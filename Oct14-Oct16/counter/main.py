cb3a6d51729c6a74a7828a104796cc2847c62370
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-10-16 10:19:30 -0600

    I LOVE NUMBERS

---
+++ Oct14-Oct16/counter/main.py
@@ -0,0 +1,12 @@
+iteration = 0
+number = 0
+
+while iteration <= 40:
+    if iteration < 20:
+        print(number)
+        number += 1
+    else:
+        print(number)
+        number -= 1
+        
+    iteration += 1
