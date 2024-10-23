887eecf0c3e616e2a28db8fa7869fc465420911c
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-10-23 10:29:40 -0600

    I am the world's best player of Rock-Paper-Scissors

--- Oct21-Oct25/rockPaperScissors/main.py
+++ Oct21-Oct25/rockPaperScissors/main.py
@@ -2,17 +2,67 @@ import random
 
 game_over = False
 
+options = [
+    "rock",
+    "paper",
+    "scissors"
+]
+
+user_score = 0
+cpu_score = 0
+
 print("Welcome to Rock, Paper, Scissors! \nAmerica's 17.5th Favorite Pastime (according to me)!")
 print('Let us begin! \nChoose one of three options. \n1. Rock \n2. Paper \n3. Scissors')
+print('If you wish to quit, write "quit"')
 
 while not game_over:
-    user_choice_string = input('What is your choice? ')
+    user_choice = input('What is your choice? ').lower()
 
-    cpu_choice = random.randint(0,2)
-    cpu_choice_string = ""
+    cpu_choice_num = random.randint(0,2)
+    cpu_choice = options[cpu_choice_num]
 
-    if user_choice_string != ["rock", "paper", "scissors"]
+    if user_choice == "quit":
+            print("Thanks for playing!")
+            game_over = True
+            continue
+
+    if user_choice not in options:
+        print("Oops! Invalid Option. Please Try Again... ")
+        continue
+
+    print(f"You played {user_choice}. \nCPU played {cpu_choice}")
+
+    if user_choice == cpu_choice:
+        print("TIE!")
+    else:
+        match user_choice:
+
+            case "rock":
+                if cpu_choice == "scissors":
+                    print("VICTORY!")
+                    user_score += 1
+                else:
+                    print("DEFEAT!")
+                    cpu_score += 1
+
+            case "paper":
+                if cpu_choice == "rock":
+                    print("VICTORY!")
+                    user_score += 1
+                else:
+                    print("DEFEAT!")
+                    cpu_score += 1
+
+            case "scissors":
+                if cpu_choice == "paper":
+                    print("VICTORY!")
+                    user_score += 1
+                else:
+                    print("DEFEAT!")
+                    cpu_score += 1
     
-    match cpu_choice:
-        case 0:
+    print(f'{user_score}-{cpu_score}')
+
+        
+
         
