b973af3e44dff08cb25127c08147ff114e85ed74
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2024-11-06 10:50:45 -0700

    ruh roh

--- November/Oct28-Nov1/quizGame/main.py
+++ November/Oct28-Nov1/quizGame/main.py
@@ -7,37 +7,61 @@ questions = [
 ]
 
 easy_questions = [
-    "What does NES stand for?",
     "Which country is Nintendo based in?",
     "Which of these companies owns the Xbox brand?",
     "What was the best selling game on the NES?",
-    ""
-]
-
-ques_one_ans = [
-    "A. Magnavox Odyssey",
-    "B. Nintendo Entertainment System",
-    "C. Atari 2600",
-    "D. Game & Watch"
+    "What is the most recent Nintendo console?"
 ]
 
 quesion_ans = [
+    # Question 1
+    "A. Magnavox Odyssey",
+    "B. Nintendo Entertainment System",
+    "C. Atari 2600",
+    "D. Game & Watch",
+    # Question 2
     "A. 1989",
     "B. 1993",
     "C. 2010",
     "D. 1983",
+    # Question 3
     "A. Sonic the Hedgehog",
     "B. Donkey Kong",
     "C. Super Mario Bros.",
     "D. The Legend of Zelda",
+    # Question 4
     "A. Playstation",
     "B. Sega Saturn",
     "C. Nintendo 64",
     "D. Sega Dreamcast",
+    # Question 5
     "A. Sega, Nintendo, Playstation",
     "B. Atari, Nintendo, Playstation",
     "C. Xbox, Nintendo, Playstation",
     "D. Sega, Nintendo, Xbox"
+]
+
+easy_question_ans = [
+    # Question 1
+    "A. United States",
+    "B. United Kingdom",
+    "C. Japan",
+    "D. China",
+    # Question 2
+    "A. Microsoft",
+    "B. Nintendo",
+    "C. Sony",
+    "D. Sega",
+    # Question 3
+    "A. Super Mario Bros.",
+    "B. Duck Hunt",
+    "C. Metroid",
+    "D. Punch-Out!!"
+    # Question 4
+    "A. Nintendo Wii",
+    "B. Nintendo Wii U",
+    "C. Nintendo 3DS",
+    "D. Nintendo Switch"
 ]
 
 user_answers = []
@@ -46,21 +70,106 @@ question = 0
 score = 100
 correct = True
 
-def report_answer(user_answer="", correct_answer=""):
-    user_answers.append(f"{questions[question]} \nYour Answer: {user_answer.upper()} \nCorrect Answer: {correct_answer.upper()}")
+def report_answer(selected_list, selected_question, user_answer="", correct_answer=""):
+    user_answers.append(f"{selected_list[selected_question]} \nYour Answer: {user_answer.upper()} \nCorrect Answer: {correct_answer.upper()}")
 
 print("~-~-~-~-~-Video Game History Quiz~-~-~-~-~-")
 
 while question < 5:
+
     if correct:
         print(questions[question])
+        for i in range(0,4):
+            print(quesion_ans[i + (4 * question)])
     else:
-        print(easy_questions[question])
-    answer = input("What is your answer? (input letter) \n").lower
+        print(easy_questions[question - 1])
+        for i in range(0,4):
+            print(easy_question_ans[i + (4 * (question))])
+
+    answer = input("What is your answer? (input letter) \n").lower()
 
     if answer not in ["a", "b", "c", "d"]:
         print("Oops! Invalid Answer. Please Try Again.")
         continue
+    
+    if question == 0:
+        
+        if answer != "a":
+            score -= 20
+            correct = False
+        else:
+            correct = True
+        report_answer(questions, question, answer, "a")
+
+    elif question == 1:
+
+        if correct:
+            if answer != "d":
+                score -= 20
+                correct = False
+            else:
+                correct = True
+            report_answer(questions, question, answer, "d")
+        else:
+            if answer != "c":
+                score -= 20
+                correct = False
+            else:
+                correct = True
+            report_answer(questions, question, answer, "c")
+
+    elif question == 2:
+
+        if correct:
+            if answer != "c":
+                score -= 20
+                correct = False
+            else:
+                correct = True
+            report_answer(questions, question, answer, "c")
+
+        else:
+
+            if answer != "a":
+                score -= 20
+                correct = False
+            else:
+                correct = True
+            report_answer(questions, question, answer, "a")
+
+    elif question == 3:
+
+        if answer != "a":
+            score -= 20
+            correct = False
+        else:
+            correct = True
+        report_answer(questions, question, answer, "a")
+
+    elif question == 4:
+
+        if correct:
+            if answer != "a":
+                score -= 20
+                correct = False
+            else:
+                correct = True
+            report_answer(questions, question, answer, "a")
+        else:
+            if answer != "d":
+                score -= 20
+                correct = False
+            else:
+                correct = True
+            report_answer(questions, question, answer, "d")
+
+    question += 1
+
+for entry in user_answers:
+    print(entry)
+
+print(f"SCORE: {score}%")
+
 
     
 
